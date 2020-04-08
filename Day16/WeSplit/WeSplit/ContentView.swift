8ce261728511f046f9aecab358a4b6a02a4d95a5
Jeremy Xue <gg831006@gmail.com>
2020-04-08 09:26:21 +0800

    🎉 (100DaysOfSwiftUI): Initail commit

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jeremy Xue on 2020/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
